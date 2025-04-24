6e356fd56d168eaea4d329254c4565c12e13dff4
Javiermartin27 <javi272m@gmail.com>
2025-04-24 02:39:35 -0300

    Update juegos.js

--- js/juegos.js
+++ js/juegos.js
@@ -1,2 +1,6 @@
+let carga = parseInt(localStorage.getItem("cargaTotal") || 10000);
+let limite = carga * 0.3;
+let gananciaUsuario = parseInt(localStorage.getItem("gananciaUsuario") || 0);
 
+// Control IA: si excede su 30%, baja la probabilidad de victoria
 console.log("Juego cargado correctamente.");
